4fb3a0b7c9d4f7bc0f908b4e1ebf311b5b697e74
Aaron Straker <aaronstraker@gmail.com>
2015-11-24 21:12:41 -0800

    updates frontpage header section markup and html; adds separation between heading/letter/convo sections

--- wp-content/themes/RokoPhoto-child/sections/heading.php
+++ wp-content/themes/RokoPhoto-child/sections/heading.php
@@ -8,7 +8,7 @@ $rokophoto_heading_text = get_theme_mod('rokophoto_child_heading');
   <div class="container">
     <div class="col-sm-12">
         <?php if(!empty($rokophoto_heading_text)) : ?>
-        <p class="heading"><?php echo $rokophoto_heading_text; ?></p>
+        <h3 class="heading"><?php echo $rokophoto_heading_text; ?></h3>
         <?php endif; ?>
     </div>
   </div>
