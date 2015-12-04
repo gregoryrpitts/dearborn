ad40189d14602aa43d21c9a0076cd3f8d427544d
Aaron Straker <aaronstraker@gmail.com>
2015-12-03 19:19:04 -0800

    updates to homepage letter section

--- wp-content/themes/RokoPhoto-child/sections/letter.php
+++ wp-content/themes/RokoPhoto-child/sections/letter.php
@@ -13,9 +13,16 @@ $rokophoto_child_letter_signature = get_theme_mod('rokophoto_child_letter_signat
 <section id="letter">
   <div class="container">
     <div class="col-sm-12">
-      <?php if(!empty($rokophoto_child_letter_title)) : ?>
-        <h3 class="heading"><?php echo $rokophoto_child_letter_title; ?></h3>
+      <div class="heading-container">
+        <?php if (!empty($rokophoto_child_letter_title_image)) : ?>
+          <div class="heading-image">
+            <?php echo '<img src="' . $rokophoto_child_letter_title_image . '">'; ?>
+          </div>
         <?php endif; ?>
+        <?php if(!empty($rokophoto_child_letter_title)) : ?>
+          <h3 class="heading-text"><?php echo $rokophoto_child_letter_title; ?></h3>
+        <?php endif; ?>
+      </div>
     </div>
     <div class="col-sm-12">
 	     <?php if(!empty($rokophoto_child_letter_paragraph_one)) : ?>
